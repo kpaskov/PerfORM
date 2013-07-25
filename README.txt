2a5ba4f37668d88073cb888db5dd95b868fbd885
kpaskov <kpaskov@stanford.edu>
2013-07-24 17:18:56 -0700

    Finished buildout.

---
+++ README.txt
@@ -0,0 +1,1 @@
+Perf ORM README
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'cx_Oracle',
+    'sqlalchemy'
+]
+
+tests_require = [
+    
+]
+
+setup(name='perf_orm',
+      version='0.0',
+      description='PERF ORM',
+      long_description=README + '\n\n' + CHANGES,
+      author='Kelley Paskov',
+      author_email='kpaskov@stanford.edu',
+      url='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = perf_orm:main
+      """,
+      )
