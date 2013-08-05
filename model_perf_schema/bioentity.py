862e2e01331c016cb7da66d75de97308710ed792
kpaskov <kpaskov@stanford.edu>
2013-08-05 10:11:04 -0700

    Add json to bioentity table.

--- model_perf_schema/bioentity.py
+++ model_perf_schema/bioentity.py
@@ -5,20 +5,21 @@ Created on Jul 24, 2013
 '''
 from model_perf_schema import Base, EqualityByIDMixin
 from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, String
+from sqlalchemy.types import Integer, CLOB, String
 
 class BioentMap(Base, EqualityByIDMixin):
     __tablename__ = 'bioent_map'
     
-    id = Column('bioent_map_id', Integer, primary_key=True)
+    id = Column('bioent_id', Integer, primary_key=True)
     format_name = Column('format_name', String)
     bioent_type = Column('bioent_type', String)
-    bioent_id = Column('bioent_id', Integer)
+    json = Column('json', CLOB)
             
-    def __init__(self, format_name, bioent_type, bioent_id):
+    def __init__(self, bioent_id, format_name, bioent_type, json):
+        self.id = bioent_id
         self.format_name = format_name
         self.bioent_type = bioent_type
-        self.bioent_id = bioent_id
+        self.json = json
             
     def unique_key(self):
         return (self.format_name, self.bioent_type)
