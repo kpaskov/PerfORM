f209588cdd9e5c4a7c39b335b8e582495d0abc95
kpaskov <kpaskov@stanford.edu>
2013-10-02 14:42:15 -0700

    Added Bioentitytabs table.

--- model_perf_schema/bioentity.py
+++ model_perf_schema/bioentity.py
@@ -4,8 +4,9 @@ Created on Jul 24, 2013
 @author: kpaskov
 '''
 from model_perf_schema import Base, EqualityByIDMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, String
+from sqlalchemy.types import Integer, String, CLOB
 
 class Bioentity(Base, EqualityByIDMixin):
     __tablename__ = 'bioentity'
@@ -25,3 +26,20 @@ class Bioentity(Base, EqualityByIDMixin):
 
     def unique_key(self):
         return (self.format_name, self.class_type)
+    
+class Bioentitytabs(Base, EqualityByIDMixin):
+    __tablename__ = 'bioentitytabs'
+    
+    bioentity_id = Column('bioentity_id', Integer, primary_key=True)
+    json = Column('json', CLOB)
+            
+    def __init__(self, bioentity_id, json):
+        self.bioentity_id = bioentity_id
+        self.json = json
+        
+    @hybrid_property
+    def id(self):
+        return self.bioentity_id
+            
+    def unique_key(self):
+        return self.bioentity_id
